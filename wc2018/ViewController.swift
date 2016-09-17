92c0955014e68c2fb5398003ccff678031ae2b31
Viktor Sydorenko <viktor.sydorenko@gmail.com>
2016-09-17 22:17:48 +0200

    Data table and basic layout

--- wc2018/ViewController.swift
+++ wc2018/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableData: UITableView!
+    
+    let cellReuseIdentifier: String = "cellGame"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell:CellGame = self.tableData.dequeueReusableCellWithIdentifier(cellReuseIdentifier) as! CellGame!
+        cell.labelTeamA.text = "AAA"
+        cell.labelTeamB.text = "BBB"
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
 }
 
---
+++ wc2018/cellGame.swift
@@ -0,0 +1,18 @@
+//
+//  cellGame.swift
+//  wc2018
+//
+//  Created by VRS on 17/09/16.
+//  Copyright © 2016 Viktor Sydorenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CellGame : UITableViewCell
+{
+    @IBOutlet weak var labelTeamA: UILabel!
+    @IBOutlet weak var labelTeamB: UILabel!
+    @IBOutlet weak var labelScore: UILabel!
+    
+}
